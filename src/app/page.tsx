e5805acca1dc920766a5d441bbb0d435e8d20e5c
João Rovetta <jv.rovettajunqueira@gmail.com>
2023-11-25 12:00:14 -0300

    + HeaderNoAuth component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
-import styles from "../styles/homeNoAuth.scss"
+import { Metadata } from "next"
+import styles from "../styles/homeNoAuth.module.scss"
+import HeaderNoAuth from "@/components/HomeNoAuth/HeaderNoAuth"
 
-export const metadata = {
-  title: "title"
+export const metadata: Metadata = {
+  title: "OneBitFlix",
+  description: "Get access to the best programming content in a simple and easy way",
+  openGraph: {
+    title: "OneBitFlix"
+  }
 }
 
 const HomeNoAuth = () => {
   return (
     <>
       <main>
-
+        <HeaderNoAuth />
       </main>
     </>
   )
---
+++ src/components/HomeNoAuth/HeaderNoAuth/index.tsx
@@ -0,0 +1,31 @@
+// Images
+import logoCta from "public/homeNoAuth/logoCta.png"
+import logoOnebitflix from "public/logoOnebitflix.svg"
+// Styles
+import styles from "./styles.module.scss"
+// Components
+import Image from "next/image"
+import Link from "next/link"
+import { Container } from "reactstrap"
+
+const HeaderNoAuth = () => {
+  return (
+    <header>
+      <div className={styles.ctaSection}>
+        <Image src={logoCta} alt="logoCta" className={styles.imgCta} />
+        <p>Register to access the courses</p>
+        <Image src={logoCta} alt="logoCta" className={styles.imgCta} />
+      </div>
+
+      <Container className={styles.nav}>
+        <Image src={logoOnebitflix} alt="logoCta" className={styles.imgLogoNav} />
+        <div className={styles.buttons}>
+          <Link className={`btn btn-outline ${styles.navBtn}`} href="/login">Log in</Link>
+          <Link className={`btn btn-outline ${styles.navBtn}`} href="/login">Create account</Link>
+        </div>
+      </Container >
+    </header>
+  )
+}
+
+export default HeaderNoAuth
